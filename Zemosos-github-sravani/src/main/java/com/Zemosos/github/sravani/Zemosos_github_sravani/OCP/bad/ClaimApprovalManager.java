4ca75ac22d4b3cde2753195ffb2cc1ace82d6194
sravani <sravani@DESKTOP-TFML27L>
2022-01-01 14:36:51 +0530

    First commit

--- Zemosos-github-sravani/src/main/java/com/Zemosos/github/sravani/Zemosos_github_sravani/OCP/bad/ClaimApprovalManager.java
+++ Zemosos-github-sravani/src/main/java/com/Zemosos/github/sravani/Zemosos_github_sravani/OCP/bad/ClaimApprovalManager.java
@@ -1,5 +1,23 @@
 package com.Zemosos.github.sravani.Zemosos_github_sravani.OCP.bad;
 
 public class ClaimApprovalManager {
+	/**
+	   * ClaimApprovalManager class responsible to approve claims this class start with processing
+	   * health claims new requirement arises to support vehicle claim we added support for process
+	   * vehicle claims in methods processVehicleClaim() this clear violation of open closed
+	   */
+	  public void processHealthClaim(HealthInsuranceSurveyor surveyor) {
+	    if (surveyor.isValidClaim()) {
+	      System.out.println(
+	          "ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
+	    }
+	  }
+
+	  public void processVehicleClaim(VehicleInsuranceSurveyor surveyor) {
+	    if (surveyor.isValidClaim()) {
+	      System.out.println(
+	          "ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
+	    }
+	  }
 
 }
